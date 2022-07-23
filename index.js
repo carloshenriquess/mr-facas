1047d75b4aada744dcede3edf5e3c43fdefb8bba
Carlos Henrique <carlos.henrique0307200@gmail.com>
2022-07-23 19:37:35 -0300

    tentativa de pegar facas do tracker.gg

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
+import { Client, GatewayIntentBits } from 'discord.js';
+import { got } from 'got';
+import { JSDOM } from 'jsdom';
+import 'dotenv/config';
 
 const client = new Client({
   intents: [
@@ -15,10 +17,30 @@ client.once('ready', () => {
   client.user.setActivity('Contando facadas');
 });
 
+// const getFacadasByNick = async nick => {
+//   const encodedNick = encodeURIComponent(nick);
+//   const url = `https://tracker.gg/valorant/profile/riot/${encodedNick}/weapons`;
+//   console.log('url:', url);
+//   const response = await got(url);
+//   const dom = new JSDOM(response.body);
+//   xpath = "//span[text()='Melee']";
+//   const matchingElement = dom.window.document.evaluate(
+//     xpath,
+//     document,
+//     null,
+//     XPathResult.FIRST_ORDERED_NODE_TYPE,
+//     null,
+//   ).singleNodeValue;
+//   const facadas =
+//     +matchingElement.parentElement.parentElement.parentElement.nextElementSibling.innerText;
+//   return facadas;
+// };
+
 client.on('messageCreate', message => {
   if (!message.content.startsWith(prefix)) return;
 
   const nick = message.content.slice(prefix.length);
+  // const facadas = getFacadasByNick(nick);
   let facadas = 0;
 
   switch (nick) {
